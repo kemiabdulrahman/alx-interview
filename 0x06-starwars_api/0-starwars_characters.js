aabeafb56c36db530165ddc46c6f5054f78d698d
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-09-11 00:09:53 +0100

    Tasks Done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charsURL = JSON.parse(body).chars;
+    const charsName = charsURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charsReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charsReqBody).name);
+        });
+      }));
+
+    Promise.all(charsName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
